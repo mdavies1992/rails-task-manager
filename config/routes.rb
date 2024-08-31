588bf3c3be3dcb88775f61ddcc75d54cf075eba7
Matthew Davies <m.davies.work@gmail.com>
2024-08-31 21:45:24 +0900

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # // The boilerplate individual non-resource routing: //
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "/tasks", to: "tasks#index", as: :tasks
+  # get "/tasks/new", to: "tasks#new", as: :new_task
+  # post "/tasks", to: "tasks#create"
+  # get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "/tasks/:id", to: "tasks#update"
+  # get "/tasks/:id", to: "tasks#show", as: :task
+  # delete "/tasks/:id", to: "tasks#destroy"
+  #
+  # // the "all in one" for CRUD routes: //
+  resources :tasks
 end
---
+++ db/migrate/20240507043107_add_validations.rb
@@ -0,0 +1,5 @@
+class AddValidations < ActiveRecord::Migration[7.1]
+  def change
+
+  end
+end
